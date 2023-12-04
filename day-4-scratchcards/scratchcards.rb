0167dd172f4e52d5751cbd67f84a345709d08477
MLN <mlnkrish86@gmail.com>
2023-12-04 21:44:27 +0530

    day 4 - part 2

--- day-4-scratchcards/scratchcards.rb
+++ day-4-scratchcards/scratchcards.rb
@@ -9,8 +9,8 @@ class Scratchcards
     lines.reduce(0) do |num, line|
       game_play_split = line.split(":")
       winners_vs_mine = game_play_split[1].split("|")
-      winning_list = winners_vs_mine[0].strip.split(" ").map {|n| n.strip.to_i}
-      my_list = winners_vs_mine[1].strip.split(" ").map {|n| n.strip.to_i}
+      winning_list = winners_vs_mine[0].strip.split(" ").map { |n| n.strip.to_i }
+      my_list = winners_vs_mine[1].strip.split(" ").map { |n| n.strip.to_i }
       value = 0
       my_list.each do |my_num|
         if winning_list.include?(my_num)
@@ -24,7 +24,30 @@ class Scratchcards
 
       num + value
     end
+  end
 
+  def copies(lines)
+    scratchcard_collection = lines.map do |line|
+      game_play_split = line.split(":")
+      winners_vs_mine = game_play_split[1].split("|")
+      winning_list = winners_vs_mine[0].strip.split(" ").map { |n| n.strip.to_i }
+      my_list = winners_vs_mine[1].strip.split(" ").map { |n| n.strip.to_i }
+      { num_copies: 1, matches: my_list.count { |n| winning_list.include?(n) } }
+    end
+
+    number_of_cards = 0
+    scratchcard_collection.each_with_index do |card, index|
+      number_of_cards = number_of_cards + card[:num_copies]
+      if card[:matches] > 0
+        (1..card[:matches]).each do |num|
+          if index + num < scratchcard_collection.length
+            scratchcard_collection[index + num][:num_copies] = scratchcard_collection[index + num][:num_copies] + card[:num_copies]
+          end
+        end
+      end
+    end
+
+    number_of_cards
   end
 end
 
@@ -34,10 +57,10 @@ File.open("input-1.txt", "r") do |f|
   puts Scratchcards.new.winning_numbers(lines)
 end
 
-# puts "Doing problem 2"
-# File.open("input-2.txt", "r") do |f|
-#   lines = f.readlines
-#   puts Gears.new.gear_ratio(lines)
-# end
+puts "Doing problem 2"
+File.open("input-2.txt", "r") do |f|
+  lines = f.readlines
+  puts Scratchcards.new.copies(lines)
+end
 
 
