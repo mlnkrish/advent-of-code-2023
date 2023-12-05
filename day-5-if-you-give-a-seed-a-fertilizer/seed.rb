e1abf1ebeadaa3960d30111eb7fdbec974fda40e
MLN <mlnkrish86@gmail.com>
2023-12-05 23:43:20 +0530

    day 5 - part 2

--- day-5-if-you-give-a-seed-a-fertilizer/seed.rb
+++ day-5-if-you-give-a-seed-a-fertilizer/seed.rb
@@ -8,7 +8,7 @@ class Mapping
   end
 
   def generate(src, dest, count)
-    @mappings << { src: src, dest: dest, count: count }
+    @mappings << { src: src, dest: dest, count: count, end: src + count - 1 }
   end
 
   def from
@@ -19,13 +19,62 @@ class Mapping
     @to
   end
 
+  def mappings
+    @mappings
+  end
+
   def transform(from)
-    mping = @mappings.find {|m| m[:src] <= from && from < m[:src] + m[:count] }
+    mping = @mappings.find { |m| m[:src] <= from && from < m[:src] + m[:count] }
     if mping.nil?
       from
     else
       mping[:dest] + (from - mping[:src])
     end
+  end
+
+  def break_down_transform(range)
+    sorted_mappings = @mappings.sort_by { |m| m[:src] }
+    to_range = []
+    sorted_mappings.each do |mapping|
+      if range[:from] == range[:to]
+        next
+      end
+
+      if range[:to] < mapping[:src]
+        next
+      end
+
+      if range[:from] >= mapping[:end]
+        next
+      end
+
+      if range[:to] == mapping[:src]
+        to_range << { from: range[:from], to: range[:to] - 1 }
+        range[:from] = range[:to]
+      end
+
+      if range[:from] < mapping[:src]
+        to_range << { from: range[:from], to: mapping[:src] - 1 }
+        range[:from] = mapping[:src]
+      end
+
+      if range[:from] >= mapping[:src]
+        if range[:to] <= mapping[:end]
+          to_range << { from: mapping[:dest] + (range[:from] - mapping[:src]), to: mapping[:dest] + (range[:to] - mapping[:src]) }
+          range[:from] = range[:to]
+        else
+          t = mapping[:dest] + mapping[:count] - 1
+          to_range << { from: mapping[:dest] + (range[:from] - mapping[:src]), to: t }
+          range[:from] = mapping[:end] + 1
+        end
+      end
+    end
+
+    if range[:from] < range[:to]
+      to_range << { from: range[:from], to: range[:to] }
+    end
+
+    to_range
   end
 
 end
@@ -42,6 +91,21 @@ class Seed
     map_from("seed", seeds, mappings).min
   end
 
+  def range_of_seeds_to_location(lines)
+    seeds_and_mappings = parse_lines_to_mappings(lines)
+    seeds = seeds_and_mappings[:seeds]
+    mappings = seeds_and_mappings[:mappings]
+
+    range_of_seeds = []
+    (0..((seeds.length / 2) - 1)).each do |idx|
+      init_value = seeds[idx * 2]
+      final_value = init_value + seeds[idx * 2 + 1] - 1
+      range_of_seeds << { from: init_value, to: final_value }
+    end
+
+    break_down_map_from("seed", range_of_seeds, mappings).sort_by { |range| range[:from] }.first[:from]
+  end
+
   private
 
   def map_from(name, src, mappings)
@@ -49,8 +113,19 @@ class Seed
     if mapping_to_use.nil?
       src
     else
+
       dest = src.map { |num| mapping_to_use.transform(num) }
       map_from(mapping_to_use.to, dest, mappings)
+    end
+  end
+
+  def break_down_map_from(name, ranges, mappings)
+    mapping_to_use = mappings.find { |mapping| mapping.from == name }
+    if mapping_to_use.nil?
+      ranges
+    else
+      dest = ranges.flat_map { |range| mapping_to_use.break_down_transform(range) }
+      break_down_map_from(mapping_to_use.to, dest, mappings)
     end
   end
 
@@ -87,10 +162,10 @@ File.open("input-1.txt", "r") do |f|
   puts Seed.new.seed_to_location(lines)
 end
 
-# puts "Doing problem 2"
-# File.open("input-2.txt", "r") do |f|
-#   lines = f.readlines
-#   puts Scratchcards.new.copies(lines)
-# end
+puts "Doing problem 2"
+File.open("input-2.txt", "r") do |f|
+  lines = f.readlines
+  puts Seed.new.range_of_seeds_to_location(lines)
+end
 
 
