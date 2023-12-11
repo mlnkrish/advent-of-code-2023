92f1ebb9ac4392d7a8940bf5cbbe0dc3030b3f3e
MLN <mlnkrish86@gmail.com>
2023-12-12 01:34:42 +0530

    day 11 - part 2

--- day-11-cosmic-expansaion/cosmic_expansion.rb
+++ day-11-cosmic-expansaion/cosmic_expansion.rb
@@ -13,30 +13,26 @@ class Universe
     @space[idx] << entity
   end
 
-  def expand
+  def expand_by(n)
     insert_rows_at = []
-    to_insert = (0..@space[0].length - 1).map { "." }
     @space.each_with_index do |line, idx|
       if line.all? { |char| char == "." }
         insert_rows_at << idx
       end
     end
-    insert_rows_at.each_with_index do |y_pos, index|
-      @space.insert(y_pos + index, to_insert.dup)
+    insert_rows_at.map { |y_pos| @galaxies.select { |galaxy| galaxy[:y] > y_pos } }.flatten.each do |galaxy|
+      galaxy[:y] = galaxy[:y] + n
     end
 
     insert_cols_at = []
-    to_insert = (0..@space[0].length - 1).map { "." }
     (0..@space[0].length - 1).each do |idx|
       if (0..@space.length - 1).map { |idy| @space[idy][idx] }.all? { |char| char == "." }
         insert_cols_at << idx
       end
     end
 
-    insert_cols_at.each_with_index do |x_pos, index|
-      @space.each do |line|
-        line.insert(x_pos + index, ".")
-      end
+    insert_cols_at.map { |x_pos| @galaxies.select { |galaxy| galaxy[:x] > x_pos } }.flatten.each do |galaxy|
+      galaxy[:x] = galaxy[:x] + n
     end
   end
 
@@ -53,7 +49,7 @@ class Universe
   def sum_galaxy_distances
     length = @galaxies.length
     @galaxies.each_with_index.reduce(0) do |acc, (galaxy_x, idx)|
-      acc + (idx+1..length-1).reduce(0) do |lacc, galaxy_jdx|
+      acc + (idx + 1..length - 1).reduce(0) do |lacc, galaxy_jdx|
         galaxy_y = @galaxies[galaxy_jdx]
         lacc + (galaxy_x[:x] - galaxy_y[:x]).abs + (galaxy_x[:y] - galaxy_y[:y]).abs
       end
@@ -72,37 +68,40 @@ class Universe
       puts ""
     end
   end
+
+  def print_galaxies
+    @galaxies.each do |galaxy|
+      puts "#{galaxy[:x]}, #{galaxy[:y]}"
+    end
+  end
 end
 
 class CosmicExpansion
   def initialize
   end
 
-  def sum_of_shortest_paths(lines)
+  def sum_of_shortest_paths(lines, expand_by)
     universe = Universe.new
     lines.each_with_index do |line, index|
       line.strip.chars.each do |char|
         universe.add_entity(index, char)
       end
     end
-    universe.expand
     universe.position_galaxies
-    # universe.print_space
+    universe.expand_by(expand_by)
     universe.sum_galaxy_distances
   end
-
-  def tiles_within_the_loop(lines) end
 
 end
 
 puts "Doing problem 1"
 File.open("input-1.txt", "r") do |f|
   lines = f.readlines
-  puts CosmicExpansion.new.sum_of_shortest_paths(lines)
+  puts CosmicExpansion.new.sum_of_shortest_paths(lines, 1)
 end
 
-# puts "Doing problem 2"
-# File.open("input-2.txt", "r") do |f|
-#   lines = f.readlines
-#   puts PipeMaze.new.tiles_within_the_loop(lines)
-# end
+puts "Doing problem 2"
+File.open("input-2.txt", "r") do |f|
+  lines = f.readlines
+  puts CosmicExpansion.new.sum_of_shortest_paths(lines, 999999)
+end
